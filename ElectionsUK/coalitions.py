8793e12cf5dd95ed89d150c50016ba91341d522a
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-11-24 19:11:01 +0100

    fixing the bug

--- ElectionsUK/coalitions.py
+++ ElectionsUK/coalitions.py
@@ -9,41 +9,42 @@ def compute_coalitions(filepath):
     party_records = df[['Party', 'Seats ']].to_records()
     party_dict = {}
     for indx, party, seats in party_records:
+        # if seats:
         party_dict[party] = seats
-    shapley_value(party_dict)
+    shapley_value(party_dict)      
+    # print(party_dict)
 
 def shapley_value(party_dict):
     all_parties = list(party_dict.keys())
     total_seats = sum(party_dict.values())
     coalition_dict = {}
-    for group_length in range(len(all_parties)):
+    print("Calculating Shapley value")
+    combinations = 0
+    for group_length in range(1, len(all_parties)):
+        print(f"Calculating the groups of lengths {group_length}")
         for permutation in it.permutations(all_parties, r=group_length):
             coalition_value = calcualte_permutation_value(
                 party_dict, permutation, total_seats)
             coalition_dict[permutation] = coalition_value
+            combinations += 1
+    print(f"Possible combinations: {combinations}")
+    print(coalition_dict)
     player_inputs = Counter()
     player_participations = Counter()
+    print("Calculating player's inputs")
     for coalition in coalition_dict:
-        if len(coalition) == 1:
-            pass
-        elif len(coalition) == 2:
-            pass
-        elif len(coalition) == 3:
-            pass
         for i, player in enumerate(coalition):
-            if i == 0:
-                playerless_coalition = coalition[:i]
-                playerless_value = 0
-                if len(playerless_coalition) != 0:
-                    playerless_coalition = coalition_dict[playerless_coalition]
-                player_inputs[player] += (playerless_coalition -
-                                          party_dict[player])
-                player_participations[player] += 1
+            playerless_coalition = coalition[:i]
+            playerless_value = 0
+            if len(playerless_coalition) != 0:
+                playerless_value = coalition_dict[playerless_coalition]
+            player_inputs[player] += (party_dict[player] - playerless_value)
+            player_participations[player] += 1
 
     for player in player_inputs:
         shapley_value = player_inputs[player] / player_participations[player]
         print(
-            f"Player {player}" + \
+            f"Player {player}: " + \
                 f"coalition input {shapley_value}"
         )
 
@@ -53,10 +54,10 @@ def calcualte_permutation_value(party_dict, permutation, total_seats):
     seats = 0
     for coalition_member in permutation:
         seats += party_dict[coalition_member]
-    if seats > 0.5 * total_seats:
-        return 1
-    else:
-        return 0
+        if seats > 0.5 * total_seats:
+            return 1
+        else:
+            return 0
 
 
 filepath = '/Users/jakubmojsiejuk/Documents/agh/game-gym/PrisonersDilemma/ElectionsUK/resources/2015.csv'
