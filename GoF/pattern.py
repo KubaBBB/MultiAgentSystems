aceee1d3e36ac5f9b10daa5de86fd3b2bbe5cf36
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-12-17 13:20:42 +0100

    further fixes - that's how it's done dude

--- GoF/pattern.py
+++ GoF/pattern.py
@@ -47,7 +47,7 @@ def count_pattern_on_grid(grid, pattern):
 
 
 def update(frameNum, img, grid, N, patterns_grid):
-    print('--- NEW ITERATION ---')
+    # print('--- NEW ITERATION ---')
     turn_statistics = Counter()
     for pname, pattern in patterns_grid:
         R, C = count_pattern_on_grid(grid, pattern)
@@ -56,7 +56,7 @@ def update(frameNum, img, grid, N, patterns_grid):
             turn_statistics[pname] += len(R)
         else:
             turn_statistics[pname] += 0    
-    print(cumulative_statistics)
+    # print(cumulative_statistics)
     for turn_pattern in turn_statistics:
         running_statistics[turn_pattern].append(turn_statistics[turn_pattern])
     # copy grid since we require 8 neighbors
@@ -94,15 +94,16 @@ def update(frameNum, img, grid, N, patterns_grid):
 
 def run_game():
     N = 500
-    grid = np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
+    p1 = 0.8
+    grid = np.random.choice(vals, N * N, p=[p1, 1.0-p1]).reshape(N, N)
 
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest')
 
     update_interval = 50
     its = 50
     animate = False 
     if animate:
+        img = ax.imshow(grid, interpolation='nearest')
         ani = animation.FuncAnimation(fig,
                                     update,
                                     fargs=(img, grid, N, patterns_list),
@@ -111,45 +112,63 @@ def run_game():
                                     interval=update_interval)
         plt.show()
     else:
-        for i in range(its):
-            update(i, None, grid, N, patterns_list)
-
+        M = 1
+        for random_inits in range(M):
+            grid = np.random.choice(vals, N * N, p=[p1, 1.0-p1]).reshape(N, N)
+            for i in range(its):
+                update(i, None, grid, N, patterns_list)
+        for pat in cumulative_statistics:
+            print(pat, cumulative_statistics[pat]/M)
 
     df = pd.DataFrame.from_dict(running_statistics)
+    df.to_csv(f'GOL-statistics-{its}_N_{N}_p_{p1}_acc.csv')
+    plot_statistics(f'GOL-statistics-{its}_N_{N}_p_{p1}_acc.csv')
 
-    df.to_csv(f'GOL-statistics-{its}_N_{N}.csv')
 
-
-# run_game()
-def plot_statistics(filename):
+def plot_statistics(filename, draw_barplot=False):
     df = pd.read_csv(filename,  index_col=0)
-    fig, ax = plt.subplots()
-
-
     window = 10
-    
-    barplot_vals, barplot_ticks = [], []
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+
+    barplot_vals, bmenas, bstds, barplot_ticks = [], [], [], []
     for pattern in df.columns:
         barplot_vals.append(df[pattern].sum())
         barplot_ticks.append(pattern)
+        bmenas.append(np.mean(df[pattern]))
+        bstds.append(np.std(df[pattern]))
         print(len(df[pattern]), pattern)
-        data = df[pattern].rolling(window=window).std()
-        ax.plot(data, label=pattern)
+        data_mean = df[pattern].rolling(window=window).mean()
+        data_std = df[pattern].rolling(window=window).std()
 
-    ax.set_title("Running statistics of patters over the iterations")
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Pattern occurences")
+        ax1.plot(data_mean, label=pattern)
+        ax2.plot(data_std, label=pattern)
+
+    ax1.set_title(f"Running mean window: {window}")
+    ax1.set_xlabel("Iteration")
+    ax1.set_ylabel("Pattern occurences")
     plt.legend()
     plt.grid()
-    # plt.show()
     svn = filename.replace('.csv', '')
-    plt.savefig(f'./GOF/{svn}_std_{window}.png')
-    
-    print(barplot_ticks)
-    fig2, ax2 = plt.subplots()
-    x = np.arange(len(barplot_ticks))
-    ax2.bar(x, barplot_vals)
-    ax2.set_xticks(x, barplot_ticks)
+    fig1.savefig(f'./GOF/{svn}_mean_{window}.png')
+
+    ax2.set_title(f"Running std window: {window}")
+    ax2.set_xlabel("Iteration")
+    ax2.set_ylabel("Pattern occurences")
+    plt.legend()
+    plt.grid()
+    svn = filename.replace('.csv', '')
+    fig2.savefig(f'./GOF/{svn}_std_{window}.png')
+
+    if draw_barplot:
+        fig3, ax3 = plt.subplots()
+        x = np.arange(len(barplot_ticks))
+        ax3.bar(x, barplot_vals, yerr=bstds)
+        ax3.set_title("Cumulative sum of patterns")
+        plt.xticks(x, barplot_ticks)
+        fig3.savefig(f'./GOF/{svn}_barplot.png')
+
     plt.show()
-        
-plot_statistics('GOL-statistics-50_N_500.csv')
+run_game()
+
+# plot_statistics(df)
